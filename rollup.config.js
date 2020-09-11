e6a86fc07cb9e12698de3085088272ff30d8c4dd
LCL121 <1391436522@qq.com>
2020-09-11 23:39:49 +0800

    Modify configuration

--- rollup.config.js
+++ rollup.config.js
@@ -57,7 +57,9 @@ const productionPlugins = [
       path.resolve(__dirname, './dev')
     ]
   }),
-  terser()
+  terser({
+    mangle: true
+  })
 ]
 
 const developmentPlugins = [
@@ -71,27 +73,21 @@ const developmentPlugins = [
 
 const productionOutput = [
   {
-    file: `./dist/${pkg.name}.js`,
+    file: `./dist/${pkg.name}.min.js`,
     format: 'umd',
     name: 'LCLImageCropper',
     sourcemap: true
   },
   {
-    file: `./dist/${pkg.name}.common.js`,
+    file: `./dist/${pkg.name}.common.min.js`,
     format: 'cjs',
     name: 'LCLImageCropper',
     sourcemap: true,
     exports: 'default'
   },
   {
-    file: `./dist/${pkg.name}.mjs`,
+    file: `./dist/${pkg.name}.min.mjs`,
     format: 'esm',
-    name: 'LCLImageCropper',
-    sourcemap: true
-  },
-  {
-    file: `./dist/${pkg.name}.iife.js`,
-    format: 'iife',
     name: 'LCLImageCropper',
     sourcemap: true
   }
